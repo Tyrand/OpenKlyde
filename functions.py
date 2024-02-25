d56bf9bd2c5deed13f5716e2ad9e0b70dda9d4fc
Tyrand <TheTyrandUK@Gmail.com>
2024-02-25 03:45:24 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,313 +1,204 @@
 import json
-import requests 
+import requests
 import os
 import asyncio
 import re
 import base64
 from PIL import Image
-import io
+from io import BytesIO  # Import BytesIO directly from the standard library
 import datetime
 
-# A function for checking bot's temperature (lterally, card temps)
 async def check_bot_temps():
+    # Function to check bot's temperature (card temps)
     process = await asyncio.create_subprocess_exec("powershell.exe", "S:\AI\extra_scripts\strippedinfo.ps1", stdout=asyncio.subprocess.PIPE)
     output, _ = await process.communicate()
     return output.decode()
 
-# Set an API struct to whatever is in a JSON file to our heart's content
 async def set_api(config_file):
-
-    # Go grab the configuration file for me
+    # Set API struct from JSON file
     file = get_file_name("configurations", config_file)
     contents = await get_json_file(file)
-    api = {}
-    
-    # If contents aren't none, clear the API and shove new data in
-    if contents != None:
+    api = {}  # Initialize the api variable
+    if contents is not None:
         api.update(contents)
-
-    # Return the API
     return api
 
-# Check to see if the API is running (pick any API)
 async def api_status_check(link, headers):
-
+    # Check if any API is running
     try:
         response = requests.get(link, headers=headers)
         status = response.ok
     except requests.exceptions.RequestException as e:
         await write_to_log("Error occurred: " + e +". Language model not currently running.")
         status = False
-
     return status
 
 def get_file_name(directory, file_name):
+    # Create file path from name and directory
+    return os.path.join(directory, file_name)
 
-    # Create the file path from name and directory and return that information
-    filepath = os.path.join(directory, file_name)
-    return filepath
-
-# Read in a JSON file and spit it out, usefully or "None" if file's not there or we have an issue 
 async def get_json_file(filename):
-
-    # Try to go read the file!
+    # Read JSON file, return content or None
     try:
         with open(filename, 'r') as file:
-            contents = json.load(file)
-            return contents
-    # Be very sad if the file isn't there to read
+            return json.load(file)
     except FileNotFoundError:
-        await write_to_log("File " + filename + "not found. Where did you lose it?")
+        await write_to_log("File " + filename + " not found. Where did you lose it?")
         return None
-    # Be also sad if the file isn't a JSON or is malformed somehow
     except json.JSONDecodeError:
         await write_to_log("Unable to parse " + filename + " as JSON.")
         return None
-    # Be super sad if we have no idea what's going on here
     except Exception as e:
         await write_to_log("An unexpected error occurred: " + e)
         return None
 
-# Write a line to the log file    
 async def write_to_log(information):
+    # Write a line to the log file
     file = get_file_name("", "log.txt")
-    
-    # Add a time stamp to the provided error message
-    current_time = datetime.datetime.now()
-    rounded_time = current_time.replace(microsecond=0)
-    text = str(rounded_time) + " " + information + "\n"
-    
+    current_time = datetime.datetime.now().replace(microsecond=0)
+    text = str(current_time) + " " + information + "\n"
     await append_text_file(file, text)
-    
-# Figure out if the user is looking for an image to be generated    
+
 def check_for_image_request(user_message):
-    # Set user's message to all lowercase
+    # Check if user is looking for an image to be generated
     user_message = user_message.lower()
-    
-    # Create a pattern we'll be matching against
     pattern = re.compile('(send|create|give|generate|draw|snap|show|take|message).*?(image|picture|photo|photogragh|pic|drawing|painting|screenshot)')
-    
-    # Do some matching, I suppose
-    result = bool(pattern.search(user_message))
-    return result
+    return bool(pattern.search(user_message))
 
-async def create_text_prompt(user_input, userName, character, bot, history, reply, text_api):
-
-    prompt = character + history + reply + userName + ": " + user_input + "\n" + bot + ": "
-    stop_sequence = ["\n" + userName + ":", userName + ":", bot + ":", "You:"]
-    
+async def create_text_prompt(user_input, user, character, bot, memory, history, reply, text_api):
+    # Create a text prompt for text generation
+    prompt = f"{character}{memory}{history}{reply}{user.name}: {user_input}. Reminder, do not reply with, nor link to, anything that would break Discord's Terms of Service.\n\n{bot}: "
+    stop_sequence = [f"{user.name}:", f"{bot}:", "You:"]
     data = text_api["parameters"]
     data.update({"prompt": prompt})
-    
-    if text_api["name"] == "openai":
-        data.update({"stop": stop_sequence})
-    else:
-        data.update({"stop_sequence": stop_sequence})
+    data.update({"stop": stop_sequence} if text_api["name"] == "openai" else {"stop_sequence": stop_sequence})
+    return json.dumps(data)
 
-    data_string = json.dumps(data)
-    return data_string
-    
 async def create_image_prompt(user_input, character, text_api):
-
+    # Create an image prompt for image generation
     user_input = user_input.lower()
-    
-    if "of" in user_input:
-        subject = user_input.split('of', 1)[1]
-        prompt = "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\nPlease describe the following in vivid detail:" + subject + "\n\n### Response:\n"
-    else:
-        prompt = "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n" + character + "Please describe yourself in vivid detail.\n\n### Response:\n"
-        
-    stop_sequence = ["### Instruction:", "### Response:", "You:" ]
-    
+    subject = user_input.split('of', 1)[1] if "of" in user_input else character + "Please describe yourself in vivid detail."
+    prompt = f"Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\nPlease describe the following in vivid detail:{subject}\n\n### Response:\n"
+    stop_sequence = ["### Instruction:", "### Response:", "You:"]
     data = text_api["parameters"]
     data.update({"prompt": prompt})
-    
-    if text_api["name"] == "openai":
-        data.update({"stop": stop_sequence})
-    else:
-        data.update({"stop_sequence": stop_sequence})
+    data.update({"stop": stop_sequence} if text_api["name"] == "openai" else {"stop_sequence": stop_sequence})
+    return json.dumps(data)
 
-    data_string = json.dumps(data)
-    return data_string
-
-# Get user's conversation history
-async def get_conversation_history(user, characters):
-
-    file_path = get_file_name("context", str(user.name) + ".txt")
-
+async def get_conversation_memory(user, characters):
+    # Get user's conversation memory
+    file_path = get_file_name("memory", f"{user.name}.txt")
     try:
-        # Read the file and put its contents into a variable
         with open(file_path, "r", encoding="utf-8") as file:
-            contents = file.readlines()
-            length = len(contents)
-            contents = contents[-characters:]
-
-            # Turn contents into a string for easier consumption
-            history_string = ''.join(contents)
-
-            # Log that the file has been accessed
+            contents = file.readlines()[-characters:]
+            memory_string = ''.join(contents)
             print("Accessed:", file_path)
+            return memory_string
+    except FileNotFoundError:
+        await write_to_log(f"File {file_path} not found. Where did you lose it?")
+        return ""
+    except Exception as e:
+        await write_to_log(f"An unexpected error occurred while accessing {file_path}: {e}")
+        return ""
 
+async def get_conversation_history(user, characters):
+    # Get user's conversation history
+    file_path = get_file_name("context", f"{user.name}.txt")
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            contents = file.readlines()[-characters:]
+            history_string = ''.join(contents)
+            print("Accessed:", file_path)
             return history_string
-
     except FileNotFoundError:
         await write_to_log(f"File {file_path} not found. Where did you lose it?")
         return None, 0
-
     except Exception as e:
         await write_to_log(f"An unexpected error occurred while accessing {file_path}: {e}")
         return None, 0
 
-async def add_to_conversation_history(message, userName, file):
-
-    file_name = get_file_name("context", file + ".txt")
-    
-    content = userName + ": " + message + "\n"
-    
+async def add_to_conversation_history(message, userName, file, user):
+    # Add message to user's conversation history
+    file_name = get_file_name("context", f"{user.name}.txt")
+    content = f"{userName}: {message}\n"
     await append_text_file(file_name, content)
 
-# Read in however many lines of a text file (for context or other text)
-# Returns a string with the contents of the file
 async def get_txt_file(filename, characters):
-
-    # Attempt to read the file and put its contents into a variable
+    # Get contents of a text file
     try:
-        with open(filename, "r", encoding="utf-8") as file:  # Open the file in read mode
-            contents = file.read()
-            length = len(contents)     
-            
-            # Fetch the last 6000 characters
-            contents = contents[-characters:]
-
-            # Find the last newline character to ensure returning full lines
+        with open(filename, "r", encoding="utf-8") as file:
+            contents = file.read()[-characters:]
             last_newline_index = contents.rfind('\n')
             if last_newline_index != -1:
                 contents = contents[last_newline_index + 1:]
-
-            # Log that the file has been accessed
             print("Accessed: ", filename)
-
-            return contents, length
-            
-    # Let someone know if the file isn't where we expected to find it.
+            return contents, len(contents)
     except FileNotFoundError:
-        await write_to_log("File " + filename + " not found. Where did you lose it?")
+        await write_to_log(f"File {filename} not found. Where did you lose it?")
         return None, 0
-    
-    # Panic if we have no idea what's going on here
     except Exception as e:
-        await write_to_log("An unexpected error occurred: " + str(e))
+        await write_to_log(f"An unexpected error occurred: {e}")
         return None, 0
-
 
 async def prune_text_file(file, trim_to):
-
+    # Prune lines from a text file
     try:
-        with open(file, "r", encoding="utf-8") as f:  # Open the file in read mode
-            contents = f.readlines()
-            contents = contents[-trim_to:]  # Keep the last 'trim_to' lines
-
-        with open(file, "w", encoding="utf-8") as f:  # Open the file in write mode
-            f.writelines(contents)  # Write the pruned lines to the file
-    
+        with open(file, "r", encoding="utf-8") as f:
+            contents = f.readlines()[-trim_to:]
+        with open(file, "w", encoding="utf-8") as f:
+            f.writelines(contents)
     except FileNotFoundError:
-        await write_to_log("Could not prune file " + file + " because it doesn't exist.")
-        
-# Append text to the end of a text file
-async def append_text_file(file, text):
+        await write_to_log(f"Could not prune file {file} because it doesn't exist.")
 
+async def append_text_file(file, text):
+    # Append text to a file
+    directory = os.path.dirname(file)
+    absolute_directory = os.path.abspath(directory)
+    if not os.path.exists(absolute_directory):
+        os.makedirs(absolute_directory)
     with open(file, 'a+', encoding="utf-8") as context:
         context.write(text)
-        context.close()
-        
-# Clean the input provided by the user to the bot!
-def clean_user_message(user_input, client):
-    # Convert bot tags to lowercase for case-insensitive matching
-    bot_tags = [re.escape(str(client.user.name)).lower(), re.escape(str(client.user.name + '#' + client.user.discriminator)).lower()]
 
-    # Convert user input to lowercase for case-insensitive matching
+def clean_user_message(user_input):
+    # Clean user input to the bot
+    bot_tags = [re.escape("@TyrandBot#6275").lower(), re.escape("@TyrandBot").lower()]
     user_input_lower = user_input.lower()
-
-    # Create a regular expression pattern for case-insensitive replacement
     pattern = re.compile("|".join(bot_tags), re.IGNORECASE)
-
-    # Replace bot tags with an empty string in a case-insensitive manner
     cleaned_input = pattern.sub("", user_input)
-
-    # Remove any spaces before and after the text.
-    cleaned_input = cleaned_input.strip()
-
-    return cleaned_input
+    return cleaned_input.strip()
 
 async def clean_llm_reply(message, userName, bot):
-    # Convert bot and user names to lowercase for case-insensitive matching
-    bot_lower = bot.lower()
-    userName_lower = userName.lower()
-
-    # Create a regular expression pattern for case-insensitive replacement
-    pattern = re.compile(re.escape(bot_lower) + r":|" + re.escape(userName_lower) + r":|You:", re.IGNORECASE)
-
-    # Replace bot and user names, as well as #bots, with an empty string in a case-insensitive manner
+    # Clean generated reply
+    bot_lower, userName_lower = bot.lower(), userName.lower()
+    pattern = re.compile(re.escape(bot_lower) + r":|" + re.escape(userName_lower) + r":|You:|#bots", re.IGNORECASE)
     cleaned_message = pattern.sub("", message)
+    return cleaned_message.strip()
 
-    # Remove any spaces before and after the text.
-    cleaned_message = cleaned_message.strip()
-
-    # Return nice and clean message
-    return cleaned_message
-
-# Get the current bot character in a prompt-friendly format
 def get_character(character_card):
-
-    # Your name is <name>.
-    character = "Your name is " + character_card["name"] + ". "
-    
-    # Your name is <name>. You are a <persona>.
-    character = character + "You are " + character_card["persona"] + ". "
-    
-    # Instructions on what the bot should do. This is where an instruction model will get its stuff.
-    character = character + character_card["instructions"]
-    
-    # Example messages!
-    character = character + "Here is how you speak: " + "\n" + '\n'.join(character_card['examples']) +"\n"
-
+    # Get current bot character in prompt-friendly format
+    character = f"Your name is {character_card['name']}. You are {character_card['persona']}. {character_card['instructions']}Here is how you speak: \n{', '.join(character_card['examples'])}\n"
     return character
-    
-# Get the contents of a character file (which should contain everything about the character)
-async def get_character_card(name):
 
-    # Get the file name and then its contents
+async def get_character_card(name):
+    # Get contents of a character file
     file = get_file_name("characters", name)
     contents = await get_json_file(file)
-    character = {}
-    
-    if contents != None:
-        character.update(contents)
+    return contents if contents is not None else {}
 
-    #return the contents of the JSON file
-    return character
-    
-# Get the list of all available characters (files in the character directory, hopefully)
 def get_file_list(directory):
-
-    # Try to get the list of character files from the directory provided. 
+    # Get list of all available characters
     try:
-        dir_path = directory + "\\"
-        files = os.listdir(dir_path)
+        dir_path, files = directory + "\\", os.listdir(dir_path)
     except FileNotFoundError:
         files = []
     except OSError:
         files = []
-
-    # Return either the list of files or a blank list.
     return files
 
 def image_from_string(image_string):
-
+    # Create an image from a base64-encoded string
     img = base64.b64decode(image_string)
-    name = "image_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + ".png"
-    
+    name = f"image_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
     with open(name, 'wb') as f:
         f.write(img)
-        
     return name
